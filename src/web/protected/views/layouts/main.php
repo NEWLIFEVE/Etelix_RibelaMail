044339203058c12c4808a95645a407a0489a3cfd
Angelo <angelo08121987@gmail.com>
2014-05-13 15:16:46 -0430

    commit

--- src/web/protected/views/layouts/main.php
+++ src/web/protected/views/layouts/main.php
@@ -14,6 +14,7 @@
 
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/responsive.css" />
         <link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/images/ribela.ico" type="image/x-icon" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
